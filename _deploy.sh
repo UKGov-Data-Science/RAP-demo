782cd5ffb88a959b5a02874732f5c391f4c5279c
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2016-11-22 13:37:57 +0000

    Initial RAP-demo commit

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "matthew.upson@digital.cabinet-office.gov.uk"
+git config --global user.name "Matthew Upson"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m "Automatic update of release" || true
 git push origin gh-pages
